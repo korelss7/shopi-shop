fa1d3476b231ac16c9129253283027118454bade
korelss7 <pajita200305@gmail.com>
2023-09-18 00:11:36 -0500

    Implementación de rutas con react-router-dom, BrowserRouter && useRoutes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,29 @@
+import { Home } from "./pages/Home";
+import { MyAccount } from "./pages/MyAccount";
+import { MyOrder } from "./pages/MyOrder";
+import { MyOrders } from "./pages/MyOrders";
+import { SignIn } from "./pages/SignIn";
+import { NotFound } from "./pages/NotFound";
+import { BrowserRouter, useRoutes } from "react-router-dom";
+
+const AppRoutes = () => {
+  const routes = useRoutes([
+    { path: "/", element: <Home /> },
+    { path: "/my-account", element: <MyAccount /> },
+    { path: "/my-order", element: <MyOrder /> },
+    { path: "/my-orders", element: <MyOrders /> },
+    { path: "/sign-in", element: <SignIn /> },
+    { path: "*", element: <NotFound /> },
+  ]);
+  return routes;
+};
+
 function App() {
-  return <div>Application</div>;
+  return (
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  );
 }
 
 export { App };
