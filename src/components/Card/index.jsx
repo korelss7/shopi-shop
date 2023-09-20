ba773e98435c67631f0e1e776db945803f6b5cf9
korelss7 <pajita200305@gmail.com>
2023-09-19 22:42:24 -0500

    Creación de Hook useFetch para manejar datos de una API

--- src/components/Card/index.jsx
+++ src/components/Card/index.jsx
@@ -1,24 +1,24 @@
-const Card = () => {
+const Card = ({ category, product, price, imageUrl }) => {
   return (
-    <div className="bg-white cursor-pointer w-56 h-60">
+    <article className="bg-white cursor-pointer w-56 h-60">
       <figure className="relative w-full h-5/6 rounded-lg overflow-hidden">
         <span className="absolute bottom-1 left-2 bg-white/60 font-normal px-2 rounded-md text-sm">
-          Electronics
+          {category}
         </span>
         <img
-          src="https://images.pexels.com/photos/3945668/pexels-photo-3945668.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-          alt="Headphones image"
+          src={imageUrl}
+          alt={`${product} image`}
           className="w-full h-full object-cover"
         />
-        <div className="absolute top-1 right-1 flex justify-center items-center bg-white w-6 h-6 rounded-full font-bold">
+        <div className="absolute top-1 right-1 flex justify-center items-center bg-white w-6 h-6 rounded-full font-bold drop-shadow-xl">
           +
         </div>
       </figure>
       <p className="flex justify-between h-1/6 items-center px-2">
-        <span className="text-sm font-light">Headphones</span>
-        <span className="text-base font-medium">$300</span>
+        <span className="text-sm font-light truncate mr-2">{product}</span>
+        <span className="text-base font-medium">${price}</span>
       </p>
-    </div>
+    </article>
   );
 };
 
--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,10 +1,28 @@
+import { useFetch } from "../../useFetch";
 import { Card } from "../../components/Card";
 
 const Home = () => {
+  const { data, loading, error } = useFetch(
+    "https://fakestoreapi.com/products"
+  );
   return (
-    <div>
-      <Card></Card>
-    </div>
+    <>
+      <h1>HOME PRODUCTS</h1>
+      <section className="flex flex-wrap justify-center gap-4 w-full max-w-5xl px-2">
+        {loading && "Cargando..."}
+        {error && "Ha ocurrido un error"}
+        {!loading &&
+          data?.map(({ title, price, category, image }) => (
+            <Card
+              key={title}
+              product={title}
+              price={price}
+              category={category}
+              imageUrl={image}
+            />
+          ))}
+      </section>
+    </>
   );
 };
 
